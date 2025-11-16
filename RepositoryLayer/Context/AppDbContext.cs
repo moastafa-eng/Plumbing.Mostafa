2d271009e9c0f0a9be0a1176fea99ab1fc8ccd49
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmial.com>
2025-11-16 22:55:38 +0200

    Generic Repository CRUD Operations (List-Add-Update-Delete)

--- RepositoryLayer/Context/AppDbContext.cs
+++ RepositoryLayer/Context/AppDbContext.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 
 namespace RepositoryLayer.Context
 {
-    internal class AppDbContext : DbContext
+    public class AppDbContext : DbContext
     {
         #region Constructors
 		
