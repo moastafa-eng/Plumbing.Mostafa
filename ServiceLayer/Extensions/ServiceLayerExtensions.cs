b36124828f1d026c3cc168888b12c49099e9261d
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmial.com>
2025-11-22 18:29:35 +0200

    Add our services in DI container

--- ServiceLayer/Extensions/ServiceLayerExtensions.cs
+++ ServiceLayer/Extensions/ServiceLayerExtensions.cs
@@ -10,6 +10,23 @@ namespace ServiceLayer.Extensions
             // Find All Configurations in Assembly file in Service layer.
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
+            // Get all non-abstract classes that end with "Service from Currently Assembly.
+            var types = Assembly.GetExecutingAssembly().GetTypes().Where(x => x.IsClass && !x.IsAbstract &&
+            x.Name.EndsWith("Service"));
+
+            // Auto-register each service with its matching interface (I + class name)
+            foreach (var serviceType in types)
+            {
+                // Try to find the interface that matches the pattern: (I + ServiceName)
+                var iServiceType = serviceType.GetInterfaces().FirstOrDefault(x => x.Name == $"I{serviceType.Name}");
+
+                // If matching interface exists, register service in DI container
+                if (iServiceType is not null)
+                {
+                    services.AddScoped(iServiceType, serviceType);
+                }
+            }
+
             return services;
         }
     }
