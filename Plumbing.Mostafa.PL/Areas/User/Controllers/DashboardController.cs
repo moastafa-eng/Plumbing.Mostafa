71c25eb964595c477538501bd524f57f7b7c1a25
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmail.com>
2025-12-13 18:59:10 +0200

    User Panel - Area & Route Setup

---
+++ Plumbing.Mostafa.PL/Areas/User/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿// Ignore Spelling: Mostafa
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Plumbing.Mostafa.PL.Areas.User.Controllers
+{
+    [Area("User")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
