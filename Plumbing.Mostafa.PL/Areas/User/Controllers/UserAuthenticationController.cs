5d59ed1a79eb3a3ab3ecf3f7afe03041b8ed38d7
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmail.com>
2025-12-14 15:31:15 +0200

    Identity Library - Advanced User Management Service - User Edit Action Method

--- Plumbing.Mostafa.PL/Areas/User/Controllers/UserAuthenticationController.cs
+++ Plumbing.Mostafa.PL/Areas/User/Controllers/UserAuthenticationController.cs
@@ -3,9 +3,12 @@
 using AutoMapper;
 using EntityLayer.Identity.Entities;
 using EntityLayer.Identity.ViewModels;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.Helpers.Identity.ModelStateHelper;
 
 namespace Plumbing.Mostafa.PL.Areas.User.Controllers
 {
@@ -15,13 +18,19 @@ namespace Plumbing.Mostafa.PL.Areas.User.Controllers
     {
         private readonly UserManager<AppUser> _usermanager;
         private readonly IMapper _mapper;
+        private readonly IValidator<UserEditVM> _userEditValidator;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public UserAuthenticationController(UserManager<AppUser> userManager, IMapper mapper)
+        public UserAuthenticationController(UserManager<AppUser> userManager, IMapper mapper,
+            IValidator<UserEditVM> userEditValidator, SignInManager<AppUser> signInManager)
         {
             _usermanager = userManager;
             _mapper = mapper;
+            _userEditValidator = userEditValidator;
+            _signInManager = signInManager;
         }
 
+        [HttpGet]
         public async Task<IActionResult> UserEdit()
         {
             var user = await _usermanager.FindByNameAsync(User.Identity!.Name!);
@@ -29,6 +38,92 @@ namespace Plumbing.Mostafa.PL.Areas.User.Controllers
             var userEditeVM = _mapper.Map<UserEditVM>(user);
 
             return View(userEditeVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserEdit(UserEditVM request)
+        {
+            var user = await _usermanager.FindByNameAsync(User.Identity!.Name!);
+
+            var validation = _userEditValidator.Validate(request);
+
+            if(!validation.IsValid)
+            {
+                validation.AddToModelState(this.ModelState);
+                return View();
+            }
+
+            var checkPassword = await _usermanager.CheckPasswordAsync(user!, request.Password);
+
+            if(!checkPassword)
+            {
+                ViewBag.Result = "Failed";
+                ModelState.AddModelErrorList(new List<string> { "Wrong Password!" });
+
+                return View();
+            }
+
+            if(request.NewPassword is not null)
+            {
+                var changePassword = await _usermanager.ChangePasswordAsync(user!, request.Password, request.NewPassword);
+
+                if(!changePassword.Succeeded)
+                {
+                    ViewBag.Result = "Failed";
+                    ModelState.AddModelErrorList(changePassword.Errors);
+
+                    return View();
+                }
+            }
+
+            var oldFileName = user!.FileName;
+            var oldFileType = user!.FileType;
+
+            if(request.Photo is not null)
+            {
+                request.FileName = DateTime.Now.ToString();
+                request.FileType = DateTime.Now.ToString();
+            }
+
+            else
+            {
+                request.FileName = oldFileName;
+                request.FileType = oldFileType;
+            }
+
+            var updateUserResult = await _usermanager.UpdateAsync(_mapper.Map(request, user));
+
+            if(updateUserResult.Succeeded)
+            {
+                if(request.Photo is not null)
+                {
+                    if(oldFileName is not null)
+                    {
+                        // Delete Image.
+                    }
+                }
+
+                await _usermanager.UpdateSecurityStampAsync(user);
+                await _signInManager.SignOutAsync();
+                await _signInManager.SignInAsync(user, false);
+
+                return RedirectToAction("Index", "Dashboard", new { Area = "User" });
+            }
+
+            if(request.FileName is not null)
+            {
+                // Image delete
+            }
+
+            if(request.NewPassword is not null)
+            {
+                await _usermanager.ChangePasswordAsync(user!, request.NewPassword, request.Password);
+                await _usermanager.UpdateSecurityStampAsync(user);
+                await _signInManager.SignOutAsync();
+                await _signInManager.SignInAsync(user, false);
+            }
+
+            return View();
         }
     }
 }
